168cf111ad0ca5835aa0156d8fa2d885aabd6905
Artem <viktorzebra54@gmail.com>
2021-11-30 21:55:26 +0300

    init

--- publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/api/model/Publication.kt
+++ publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/api/model/Publication.kt
@@ -1,5 +1,16 @@
 package com.lurking.cobra.blog.publication.service.api.model
 
+import com.lurking.cobra.blog.publication.service.api.model.dto.PublicationDto
+import com.lurking.cobra.blog.publication.service.api.model.dto.PublicationMapper
+import com.lurking.cobra.blog.publication.service.api.model.entity.PublicationEntity
+
 // mapstruct
-class Publication {
+class PublicationMapperImp : PublicationMapper {
+    override fun convertToDto(publicationEntity: PublicationEntity): PublicationDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun convertToModel(publicationDto: PublicationDto): PublicationEntity {
+        TODO("Not yet implemented")
+    }
 }
---
+++ publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/impl/repository/PublicationRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.lurking.cobra.blog.publication.service.impl.repository
+
+import com.lurking.cobra.blog.publication.service.api.repository.PublicationRepository
+
+class PublicationRepositoryImpl : PublicationRepository
