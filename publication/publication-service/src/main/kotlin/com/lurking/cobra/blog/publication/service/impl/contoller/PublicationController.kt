d2eae56bee19d912a19272fef88fd9596a4e33a0
1Bitcoin <shigalkin@mail.ru>
2021-11-30 23:28:44 +0300

    Переименовал контроллер

--- publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/impl/contoller/PublicationController.kt
+++ publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/impl/contoller/PublicationController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*
 
 
 @RestController
-class PublicationServiceController {
+class PublicationController {
     @GetMapping("/post")
     fun getPublicationForPost(): PublicationDto {
         return PublicationDto()
