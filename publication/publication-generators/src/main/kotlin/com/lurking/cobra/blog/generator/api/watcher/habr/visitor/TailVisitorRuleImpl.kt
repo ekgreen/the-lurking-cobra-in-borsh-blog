81c1418f8cf8fc476df27ee757956dc2733c4c28
Danil Chernykh <degget.dufus@gmail.com>
2021-12-01 23:27:24 +0300

    переименовал прошлую реализацию в DayLimitTailVisitorRule

--- publication/publication-generators/src/main/kotlin/com/lurking/cobra/blog/generator/api/watcher/habr/visitor/TailVisitorRuleImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.lurking.cobra.blog.generator.api.watcher.habr.visitor
-
-import com.lurking.cobra.blog.generator.api.publication.Publication
-
-class TailVisitorRuleImpl : TailVisitorRule {
-    override fun isTailPublication(publication: Publication): Boolean {
-        TODO("Not yet implemented")
-    }
-}
