0b7c8e1ee508b321c92332429777089bda2ed7d2
Artem <viktorzebra54@gmail.com>
2021-12-02 18:04:36 +0300

    захадкодил статус публикации в запросе числом(2), ${Status.status_name} - не работает

--- publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/api/repository/PublicationRepository.kt
+++ publication/publication-service/src/main/kotlin/com/lurking/cobra/blog/publication/service/api/repository/PublicationRepository.kt
@@ -11,12 +11,13 @@ import com.lurking.cobra.blog.publication.service.api.model.entity.Status
 interface PublicationRepository : MongoRepository<PublicationEntity, String> {
 
     // max(rating) where status == PUBLICATION _READY && текущая_дата - месяц <= last_publication_timestamp limit count < .count
-    @Query("db.publication.find({'status': ${Status.PUBLICATION_READY}, 'last_publication': null}).sort({'rating':-1}).limit(?0)")
+    // статус константы надо пофиксить по хорошему, выражение ${Status.READY_PUBLICATION} - не работает почему то
+    @Query("db.publication.find({'status': 2, 'last_publication': null}).sort({'rating':-1}).limit(?0)")
     fun findPublicationThisMonth(count: Int): MutableList<PublicationEntity>
 
     //{$lte: new Date(ISODate().getTime() - 1000 * 60 * 60 * 24 * 30)}
     // статьи за последний год имеющие наименьшее кол-во публикаций
-    @Query("db.publication.find({'status': ${Status.PUBLICATION_READY}, 'last_publication': {\$lte:new Date(ISODate().getTime() - 1000 * 60 * 60 * 24 * 30),\$gt:new Date(ISODate().getTime() - 1000 * 60 * 60 * 24 * 180)}}).sort({'rating':-1, 'count_publication':1}).limit(?0)")
+    @Query("db.publication.find({'status': 2, 'last_publication': {\$lte:new Date(ISODate().getTime() - 1000 * 60 * 60 * 24 * 30),\$gt:new Date(ISODate().getTime() - 1000 * 60 * 60 * 24 * 180)}}).sort({'rating':-1, 'count_publication':1}).limit(?0)")
     fun findPublicationThisYear(count: Int): MutableList<PublicationEntity>
 
     // инкриминируем count_publication
